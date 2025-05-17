d4a4685ccbae53e05c6512259b540133f073773d
ayanrajpoot10 <ayanrajpoot2004@gmail.com>
2025-05-17 22:40:23 +0530

    refactor: Remove commented-out code from Proxy2Scanner constructor

--- bugscanx/modules/scanners_pro/scanners/proxy_request.py
+++ bugscanx/modules/scanners_pro/scanners/proxy_request.py
@@ -4,17 +4,6 @@ from .direct import DirectScanner
 
 class Proxy2Scanner(DirectScanner):
 
-    # def __init__(self, proxy=None, auth=None):
-    #     super().__init__()
-    #     self.proxy = proxy or {}
-    #     self.auth = auth
-    #     self.session = requests.Session()
-        # if self.proxy:
-        #     self.session.proxies.update(self.proxy)
-        # if self.auth:
-        #     self.session.auth = self.auth
-        # self.requests = self.session
-
     def __init__(self, proxy=None, auth=None, method_list=None, host_list=None, port_list=None, no302=False, is_cidr_input=False, task_list=None, threads=None):
         super().__init__(method_list=method_list, host_list=host_list, port_list=port_list,
                      no302=no302, is_cidr_input=is_cidr_input,
@@ -27,7 +16,6 @@ class Proxy2Scanner(DirectScanner):
         if self.auth:
             self.session.auth = self.auth
         self.requests = self.session
-
 
     def set_proxy(self, proxy, username=None, password=None):
 
